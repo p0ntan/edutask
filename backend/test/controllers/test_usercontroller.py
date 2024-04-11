556a0cf9a5089d60d7d3a991ef5b235e3d78c08d
p0ntan <pontus.akerberg@hotmail.com>
2024-04-11 15:39:44 +0200

    Add more test for assignment 2
    Co-authored-by: idaloof <idaloof@users.noreply.github.com>

--- backend/test/controllers/test_usercontroller.py
+++ backend/test/controllers/test_usercontroller.py
@@ -3,7 +3,7 @@ import unittest.mock as mock
 from src.controllers.usercontroller import UserController
 
 @pytest.fixture
-def user_controller(dao_response: list=None):
+def user_controller(dao_response: list):
     mockedDao = mock.MagicMock()
     mockedDao.find.return_value = dao_response
     controller = UserController(mockedDao)
@@ -11,20 +11,29 @@ def user_controller(dao_response: list=None):
 
 @pytest.mark.assignment_2
 class TestGetUserByEmail:
-    # @pytest.mark.parametrize('dao_response, expected', [
-    #     ([], None),
-    #     ([{'name': 'First Dave', 'email': 'valid_user@test.com'}], [{'name': 'First Dave', 'email': 'valid_user@test.com'}])
-    # ])
-    # def test_get_user_valid_email(self):
-    #     """"""
+    @pytest.mark.parametrize('dao_response, expected', [
+        ([], None),
+        ([{'name': 'First Dave', 'email': 'valid_user@test.com'}],{'name': 'First Dave', 'email': 'valid_user@test.com'}),
+        ([{'name': 'First Dave', 'email': 'valid_user@test.com'}, {'name': 'Second Dave', 'email': 'valid_user@test.com'}], {'name': 'First Dave', 'email': 'valid_user@test.com'}),
+        ([{'name': 'First Dave', 'email': 'valid_user@test.com'}, {'name': 'Second Dave', 'email': 'valid_user@test.com'}, {'name': 'Third Dave', 'email': 'valid_user@test.com'}], {'name': 'First Dave', 'email': 'valid_user@test.com'})
+    ])
+    def test_get_user_valid_email(self, user_controller, expected):
+        validation_result = user_controller.get_user_by_email(email='valid_user@test.com')
+        assert validation_result == expected
 
+    @pytest.mark.parametrize('dao_response, expected', [
+        ([{'name': 'First Dave', 'email': 'valid_user@test.com'}, {'name': 'Second Dave', 'email': 'valid_user@test.com'}], 'Error: more than one user found with mail valid_user@test.com\n'),
+        ([{'name': 'First Dave', 'email': 'valid_user@test.com'}, {'name': 'Second Dave', 'email': 'valid_user@test.com'}, {'name': 'Third Dave', 'email': 'valid_user@test.com'}], 'Error: more than one user found with mail valid_user@test.com\n')
+    ])
+    @pytest.mark.testlest
+    def test_multiple_users(self, user_controller, expected, capsys):
+        user_controller.get_user_by_email(email='valid_user@test.com')
+        captured = capsys.readouterr()
+
+        assert captured.out == expected
 
     @pytest.mark.parametrize('email', [
         ('mail@missingdot'),
-        ('missingat.com'),
-        ('@missinglocal.com'),
-        ('local@.missingdomain'),
-        ('local@missinghost.'),
         (None)
     ])
     def test_invalid_email(self, email):
@@ -33,4 +42,3 @@ class TestGetUserByEmail:
         invalid_email = email
         with pytest.raises(ValueError):
             controller.get_user_by_email(invalid_email)
-
