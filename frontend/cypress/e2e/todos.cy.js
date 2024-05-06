6602f1eeeb2da3161c0692fd59cd4ab36e16855d
p0ntan <pontus.akerberg@hotmail.com>
2024-05-06 13:16:58 +0200

    Restructure test, changing arrangments beforeEach

--- frontend/cypress/e2e/todos.cy.js
+++ frontend/cypress/e2e/todos.cy.js
@@ -1,12 +1,11 @@
-describe('Interact with todos on an existing task', () => {
+describe('Interact with todos on an existing task with two todos', () => {
   // define variables that we need on multiple occasions
   let uid // user id
-  let name // name of the user (firstName + ' ' + lastName)
   let email // email of the user
-  let taskId // taskId
 
-  before(function () {
-    // Create a test-user
+  beforeEach(function () {
+    // Add a user and task for test-user before each test
+    cy.viewport(1366,	768)
     cy.fixture('user.json')
       .then((user) => {
         cy.request({
@@ -16,39 +15,42 @@ describe('Interact with todos on an existing task', () => {
           body: user
         }).then((response) => {
           uid = response.body._id.$oid
-          name = user.firstName + ' ' + user.lastName
           email = user.email
+
+          cy.fixture('test-task.json')
+            .then((task) => {
+              cy.request({
+                method: 'POST',
+                form: true,
+                url: 'http://localhost:5000/tasks/create',
+                body: {
+                  userid: uid,
+                  ...task
+                }
+              })
+            }).then((res) => {
+                cy.request({
+                  method: 'POST',
+                  form: true,
+                  url: 'http://localhost:5000/todos/create',
+                  body: {
+                    taskid: res.body[0]._id.$oid,
+                    description: 'A done todo!',
+                    done: true
+                  }
+              }).then(() => {
+                  cy.visit('http://localhost:3000')
+                    .get('.inputwrapper #email')
+                    .type(email)
+                    .get('form')
+                    .submit()
+                    .get('.container-element a').click()
+              })
+            })
         })
       })
   })
-
-  beforeEach(function () {
-    // Add task for test-user before each test
-    // Contains one todo "Hold my beer and watch this"
-    cy.fixture('test-task.json')
-    .then((task) => {
-      cy.request({
-        method: 'POST',
-        form: true,
-        url: 'http://localhost:5000/tasks/create',
-        body: {
-          userid: uid,
-          ...task
-        }
-      }).then((response) => {
-        taskId = response.body[0]._id.$oid
-      })
-    })
-
-    // Go to the detalied view for a task
-    cy.visit('http://localhost:3000')
-      .get('.inputwrapper #email')
-      .type(email)
-      .get('form')
-      .submit()
-      .get('.container-element a').click()
-  })
-
+  
   it('form should have an empty input-field and a disabled button', () => {
     cy.get('.popup form input[type=text]')
       .should('have.value', '')
@@ -57,7 +59,7 @@ describe('Interact with todos on an existing task', () => {
   })
 
   it('should add a todo to bottom of list', () => {
-    let textString = "This is a todo for testing"
+    let textString = 'This is a todo for testing'
 
     cy.get('.popup form input[type=text]')
       .type(textString)
@@ -70,65 +72,37 @@ describe('Interact with todos on an existing task', () => {
   })
 
   it('should mark the a todo as done with a strike through', () => {
-    cy.get('li.todo-item')
-      .first()
-      .find('.checker')
-      .click()
+    cy.get('li.todo-item').first().as('todoItem')
 
-    cy.wait(100)
+    cy.get('@todoItem').find('.checker').click()
 
-    cy.get('li.todo-item')
-      .first()
+    cy.get('@todoItem')
       .find('.editable')
-      .invoke('css', 'text-decoration')
-      .should('include', 'line-through')
+      .should('have.css', 'text-decoration-line', 'line-through')
   })
 
   it('should mark the a todo as active and remove strike through', () => {
-    cy.get('li.todo-item')
-      .first()
-      .find('.checker')
-      .click()
+    cy.get('li.todo-item').last().as('todoItem')
 
-    cy.wait(100)
+    cy.get('@todoItem').find('.checker').click()
 
-    cy.get('li.todo-item')
-      .first()
-      .find('.checker')
-      .click()
-
-    cy.wait(100)
-
-    cy.get('li.todo-item')
-      .first()
+    cy.get('@todoItem')
       .find('.editable')
-      .invoke('css', 'text-decoration')
-      .should('not.include', 'line-through')
+      .should('not.have.css', 'text-decoration-line', 'line-through')
   })
 
-  it('should delete the todo', () => {
+  it('should delete one todo, with one todo left in list', () => {
     cy.get('li.todo-item')
       .last()
       .find('.remover')
       .click()
 
-    cy.wait(100)
-
     cy.get('li.todo-item')
-      .should('not.exist')
+      .its('length')
+      .should('eq', 1)
   })
 
   afterEach(function () {
-    // Remove task after each testcase
-    cy.request({
-      method: 'DELETE',
-      url: `http://localhost:5000/tasks/byid/${taskId}`
-    }).then((response) => {
-      cy.log(response.body)
-    })
-  })
-
-  after(function () {
     // clean up by deleting the user from the database
     cy.request({
       method: 'DELETE',
